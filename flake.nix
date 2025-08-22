f501fb88630620debed44ed7198d55052eda1611
Julien Thomas <juthomas@tandemdiabetes.com>
2025-08-22 13:44:32 +0200

    Add flake to allow nix devenv

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "{{NAME}}";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      flake-utils,
+      nixpkgs,
+      rust-overlay,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells.default =
+          with pkgs;
+          mkShell {
+            buildInputs = [
+              openssl
+              pkg-config
+              loco
+              sea-orm-cli
+
+              (rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
+            ];
+
+            shellHook = '''';
+          };
+      }
+    );
+}
